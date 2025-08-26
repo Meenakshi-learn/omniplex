689940aff37fad9e05bbefaa043d09eb547c96e1
Meenakshi <meenakshi.herenow@gmail.com>
2025-08-26 15:13:14 +0530

    Fix workflow errors and prepare for Azure deployment - By Meenakshi

--- src/app/api/stripe-checkout/route.ts
+++ src/app/api/stripe-checkout/route.ts
@@ -1,9 +1,13 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+/* const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
-});
+}); */
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-07-30.basil",
+}); /* updated to support latest version */
 
 export async function POST(req: Request) {
   try {
